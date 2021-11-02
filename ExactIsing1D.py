Add Jastrow model, correct gradient of Mean Field

ExactIsing1D.py
@@ -29,7 +29,7 @@ class ExactIsing1D:
 		self.h = self.create_h(type_of_h)
 		self.partition = None
 
-	#@jit(nopython=True)
+
 	def create_J(self, type_of_J):
 		'''
 		Creates the interaction matrix J for the 1D chain
@@ -126,7 +126,7 @@ class ExactIsing1D:
 		
 		return self.partition
 
-	def boltzmannn(self, sample):
+	def probability(self, sample):
 		'''
 		Computes the Boltzmann probability of the state of the sample
 		Input:
@@ -180,3 +180,4 @@ class ExactIsing1D:
 		l2_l1 = 2*np.sinh(2*self.beta*J)/l1**2
 
 		return -self.n_samples*np.log(l1)/self.beta - np.log( 1+l2_l1**self.n_samples)/self.beta
+		#return - np.log(l1**self.n_samples + l2**self.n_samples)/self.beta

Jastrow.py
@@ -3,9 +3,9 @@ from numba import jit
 import ExactIsing1D
 
 
-class MeanField:
+class Jastrow:
 	'''
-	Computes the quantities in the Mean Field approximation H = - sum_k b_k s_k
+	Computes the quantities in the Jastrow functions model H = - sum_ si Wij sj
 	'''	
 
 	########################################
@@ -15,7 +15,7 @@ class MeanField:
 		Initialization
 		beta (scalar): inverse temperature of the system
 		n_samples (int): number of samples
-		parameters (1D array): parameters {b_i} of the system
+		parameters (2D array): parameters {W_ij} of the system (upper diagonal)
 		type_of_j (string): type of interaction defined by J
 						'zero' : J=0
 						'nearest_neighboors' : J!=0 if two neighboors (with same intensity)
@@ -25,53 +25,8 @@ class MeanField:
 		'''
 		self.beta = beta
 		self.n_samples = n_samples
-		self.parameters = np.ones(n_samples)
-		self.J = self.create_J(type_of_J)
-		self.h = self.create_h(type_of_h)
-
+		self.parameters = np.triu( np.ones( (n_samples, n_samples) ), k=1)
 		self.exact_model = ExactIsing1D.ExactIsing1D(beta, n_samples, type_of_J, type_of_h)
-
-	def create_J(self, type_of_J):
-		'''
-		Creates the interaction matrix J for the 1D chain
-		type_of_j (string): type of interaction defined by J
-							'zero' : J=0
-							'nearest_neighboors' : J!=0 if two neighboors (with same intensity)
-
-		Return : J(2D matrix) : interaction matrix
-		'''
-		J = np.zeros( (self.n_samples, self.n_samples) )
-
-		if type_of_J=='zero':
-			return J
-		elif type_of_J == 'nearest_neighboors':
-			for i in range(1,self.n_samples-1):
-				J[i, i-1] = 1
-				J[i, i+1] = 1
-			J[0, -1] = 1
-			J[0, 1] = 1
-			J[-1, 0] = 1
-			J[-1, -2] = 1
-
-		return J
-
-	def create_h(self, type_of_h):
-		'''
-		Creates the magnetic field vector h
-		size (int): number of spins (gives vector of sizeX1)
-		type_of_h (string): type of magnetic field defined by h
-							'zero' : h=0
-							'homogeneous' : h=1
-							'peak' : h[position] = 10, otherwise h=1
-
-		Return : J(2D matrix) : interaction matrix
-		'''
-		if type_of_h=='homogeneous':
-			return np.ones(self.n_samples)
-		elif type_of_h=='zero':
-			return np.zeros(self.n_samples)
-
-		return h
 
 	def print_infos(self):
 		'''
@@ -79,7 +34,8 @@ class MeanField:
 		'''
 		print(f'Inverse temperature of the system : {self.beta}')
 		print(f'The actual parameters are {self.parameters}')
-		print(f'The field is {self.h} and the interactions \n {self.J}')
+		print(f'The exact model is :')
+		self.exact_model.print_infos()
 		print('-----------------------------------------')
 
 
@@ -89,18 +45,22 @@ class MeanField:
 		'''
 		Computes the energy of the system
 		Input:
-		sample (1D array): spins {sigma_i} of the sample 
+		sample (1D array): spins {s_i} of the sample 
 
 		Return : total energy (scalar)
 		'''
-		return -sum( np.multiply(sample,self.parameters) )
-
+		E = 0
+		for i in range(self.n_samples):
+			for j in range(self.n_samples):
+				if i<j:
+					E += sample[i]*self.parameters[i,j]*sample[j]
+		return E
 
 	def unnormalized_log_probability(self, sample):
 		'''
 		Computes the log Boltzmann probability of the unnormalized energy
 		Input:
-		sample (1D array): spins {sigma_i} of the sample 
+		sample (1D array): spins {s_i} of the sample 
 		
 		Return : unnormalized logarithm of probability(scalar)
 		'''
@@ -113,17 +73,22 @@ class MeanField:
 
 		Return : partition function (scalar)
 		'''
-		Z = 2**self.n_samples
-		for b in self.parameters:
-			Z *= np.cosh(self.beta*b)
+		a = 1
+		b = 1
+		for i in range(self.n_samples):
+			for j in range(self.n_samples):
+				if i<j:
+					a *= np.cosh(-self.beta*self.parameters[i,j])
+					b *= np.sinh(-self.beta*self.parameters[i,j])
 
+		Z = 2**self.n_samples*(a+b)
 		return Z
 
 	def log_probability(self, sample):
 		'''
 		Computes the log proabability of the sample
 		Input :
-		sample (1D array): spins {sigma_i} of the sample 
+		sample (1D array): spins {s_i} of the sample 
 
 		Return : log probability (scalar)
 		'''
@@ -133,7 +98,7 @@ class MeanField:
 		''' 
 		Computes the local free energy F_loc
 		Input :
-		sample (1D array): spins {sigma_i} of the sample 
+		sample (1D array): spins {s_i} of the sample 
 
 		Return : F_loc (scalar)
 		'''
@@ -148,11 +113,26 @@ class MeanField:
 		Input :
 		sample (1D array): spins {s_i} of the sample 
 
-		Return : gradient (1D array)
+		Return : gradient (2D array upper-diagonal)
 		'''
-		grad = np.zeros(self.n_samples)
+		grad = np.zeros( (self.n_samples, self.n_samples) )
+		a = 1
+		b = 1
+
+		# Compute denominator
+		for i in range(self.n_samples):
+			for j in range(self.n_samples):
+				if i < j:
+					a *= np.cosh(-self.beta*self.parameters[i,j])
+					b *= np.sinh(-self.beta*self.parameters[i,j])
+
+		# Gradient for W_kl
 		for k in range(self.n_samples):
-			grad[k] = - np.tanh(self.beta*self.parameters[k]) + sample[k]
+			for l in range(self.n_samples):
+				if k < l:
+					grad[k,l] = a*np.sinh(-self.beta*self.parameters[k,l])/np.cosh(-self.beta*self.parameters[k,l]) + b*np.cosh(-self.beta*self.parameters[k,l])/np.sinh(-self.beta*self.parameters[k,l])
+					grad[k,l] /= a+b
+					grad[k,l] -= sample[k]*sample[l]
 
 		return self.beta*grad
 
@@ -162,7 +142,7 @@ class MeanField:
 		Input:
 		list_of_samples (2D array): all the samples on which to average
 
-		Return : gradient (1D array)
+		Return : gradient (2D array) (upper-diagonal)
 		'''
 		# Number of samples
 		ns = list_of_samples[:,0].size

Test_MeanFieldModel.py
@@ -24,9 +24,42 @@ test = MF.MeanField(beta=1, n_samples=30)
 # MF energy = -sum b_k s_k with b_k = 1 by default
 print(f'Mean field energy should be {-30} and is {test.energy(sample)}')
 
-test.update_parameters(-test.parameters*3.0)
+test.parameters = -test.parameters*3.0
 print(f'Mean field energy should be {90} and is {test.energy(sample)}')
 
-print(test.GD(1e-3))
 
 print('-------------------------------')
+print('-------------------------------')
+
+# Testing partition function
+beta = 1e-2
+for n_spins in [3,5,7,10,12,15]:
+	test_partition = MF.MeanField(beta=beta, n_samples=n_spins)
+	s_tuples = np.array(list(k for k in itertools.product( [1.0, -1.0], repeat=n_spins)))
+	#print(s_tuples)
+	Z = 0
+	for s in s_tuples:
+		Z += np.exp(-beta*test_partition.energy(s))
+
+	print(f'n={n_spins}, real Z={Z} and implemented Z={test_partition.partition_function()}')
+
+
+# Testing gradient
+test_grad = MF.MeanField(beta=10, n_samples=5)
+s = np.random.choice([-1.0, 1.0], test_grad.n_samples)
+#s = np.ones(test_grad.n_samples)
+
+df_dx = np.zeros( test_grad.n_samples)
+for x0 in [0.1, 0.01, 0.001, 0.0001]:
+	f = test_grad.log_probability(s)
+	for k in range(test_grad.n_samples):
+		test_grad.parameters[k] = test_grad.parameters[k] + x0
+
+		f_plus = test_grad.log_probability(s)
+
+		df_dx[k] = (f_plus - f)/x0
+
+		test_grad.parameters[k] = test_grad.parameters[k] - x0
+
+	print(f'la diff finie vaut {df_dx} \n et la dérivée {test_grad.local_gradient(s)}')
+

VMC_Jastrow.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import MCMC
-import MeanField as MF
+import Jastrow as JS
 import ExactIsing1D
 
 from scipy.optimize import fsolve
@@ -9,18 +9,18 @@ from scipy.optimize import fsolve
 ##########################################################
 # Parameters
 #Number of particles
-n_spins = 25
+n_spins = 10
 
 #Temperature
 beta = 1e0 #[eV]
 
-n_variational_loops = 2000
+n_variational_loops = 10000
 
 learning_rate = 1e-1
 size_of_mean = 500
 
 
-model = MF.MeanField(beta, n_samples=n_spins)
+model = JS.Jastrow(beta, n_samples=n_spins)
 exact_model = ExactIsing1D.ExactIsing1D(beta, n_samples=n_spins)
 
 engine = MCMC.MCMC(model, warm_up_iterations=5000)
@@ -32,7 +32,7 @@ engine.print_infos()
 
 F_loc = np.zeros(n_variational_loops+1)
 F_lamb = np.zeros(n_variational_loops+1)
-parameters_memory = np.zeros( (n_variational_loops, model.n_samples) )
+parameters_memory = np.zeros( (n_variational_loops, model.n_samples, model.n_samples) )
 
 
 # Variational computation w.r.t. parameters {b_i}
@@ -78,43 +78,44 @@ for i in range(n_variational_loops):
 
 	# Update with new parametres
 	model.parameters = model.parameters-learning_rate*grad
-	parameters_memory[i,:] = model.parameters
+	parameters_memory[i,:,:] = model.parameters
 
 
 
 
-#print(f'b is {model.parameters}')
+print(f'W is {model.parameters}')
 ###############################################
 # Graphics
 J=1
 h=1
-fct = lambda b_ana : b_ana - h - 2*J*np.tanh(beta*b_ana)
-b_sol = fsolve(fct, 2.9)
-F_lamb_exact = -model.n_samples*( np.log( 2*np.cosh(model.beta*b_sol)) )/model.beta
-
 F_ising = exact_model.free_energy()
 
+param_memory = [np.ndarray.flatten(parameters_memory[k,:,:]) for k in range(n_variational_loops)]
 
 # Evolutiono of the parameters
 plt.figure()
-plt.plot(range(n_variational_loops), parameters_memory)
-plt.axhline(y=b_sol, color='k', ls=':', label=r'Theoretical solution $b_0$')
+for k in range(model.n_samples):
+	for l in range(model.n_samples):
+		if k < l:
+			plt.plot(range(n_variational_loops), parameters_memory[:,k,l], label=f'W_{k},{l}')
+#plt.axhline(y=b_sol, color='k', ls=':', label=r'Theoretical solution $b_0$')
 plt.xlabel('Iteration')
-plt.ylabel('Parameters b_i')
-plt.legend()
+plt.ylabel('Parameters W_ij')
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
 plt.title(f'Variational with lr = {learning_rate}, beta = {beta} and {n_spins} spins')
+plt.savefig("VMC_Jastrow_parameters(lr=1e-1).png", bbox_inches="tight")
 
 
 
 plt.figure()
-plt.plot(range(n_variational_loops+1), F_loc, 'b', label = r'$\mathcal{F}_{loc}(\{ b_i \})$')
+plt.plot(range(n_variational_loops+1), F_loc, 'b', ls=':', label = r'$\mathcal{F}_{loc}(\{ b_i \})$')
 plt.plot(range(n_variational_loops+1), F_lamb, 'b', label = r'$\mathcal{F}_{\lambda}(\{ b_i \})$')
-plt.axhline(y=F_lamb_exact, color='r', ls=':', label=r'$\mathcal{F}_{\lambda}(\{ b_0 \})$')
+#plt.axhline(y=F_lamb_exact, color='r', ls=':', label=r'$\mathcal{F}_{\lambda}(\{ b_0 \})$')
 plt.axhline(y=F_ising, color='g', ls=':', label=r'$\mathcal{F}(\{ b_0 \})$')
 plt.ylabel('Free Energy')
-plt.legend()
+plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
 plt.title(f'Variational with lr = {learning_rate}, beta = {beta} and {n_spins} spins')
-
+plt.savefig("VMC_Jastrow_freeEnergy(lr=1e-1).png", bbox_inches="tight")
 
 
 