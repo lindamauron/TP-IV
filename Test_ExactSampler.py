4d79461a8353ea16d4e9c6278b5bf92d844f82ad
Linda Mauron <linda.mauron@epfl.ch>
2021-11-22 09:01:11 +0100

    Add exact sampler

---
+++ Test_ExactSampler.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ExactSampler
+import Jastrow as JS
+import MeanField as MF
+
+from scipy.optimize import fsolve
+###############################################
+# Parameters
+#Number of particles
+n_spins = 10
+
+#Temperature
+beta = 1e0 #[eV]
+
+model = MF.MeanField(beta, n_samples=n_spins)
+
+'''
+J=1
+h=1
+fct = lambda b_ana : b_ana - h - 2*J*np.tanh(beta*b_ana)
+b_sol = fsolve(fct, 2.9)
+model.parameters = b_sol*np.ones(n_spins)
+'''
+
+engine = ExactSampler.ExactSampler(model, iterations=1000)
+engine.print_infos()
+engine.show_distribution()
+
+###############################################
+
+#Creating vector of spins, randomly +1 or -1
+
+
+# Loop
+samples_memory = engine.run( )
+
+
+print('Loops done')
+###############################################
+# Treating data
+
+
+energy_of_sample = np.zeros(samples_memory[:,0].size)
+f_loc = np.zeros(samples_memory[:,0].size)
+for k in range(energy_of_sample.size):
+	energy_of_sample[k] = model.energy(samples_memory[k,:])
+	f_loc[k] = model.local_free_energy(samples_memory[k,:])
+
+
+
+
+E_mean = energy_of_sample.mean()
+
+F_ising = model.exact_model.free_energy()
+###############################################
+# Graphics
+
+
+
+plt.figure()
+plt.plot(range(energy_of_sample.size), energy_of_sample, 'o')
+plt.xlabel('Iteration')
+plt.ylabel("Energy")
+plt.title(f"{n_spins} spins, {5000} loops, Mean energy = {E_mean}, beta = {beta}")
+
+
+#Compute density of states
+#E_theoretical, DOS = model.DOS()
+
+plt.figure()
+plt.hist(energy_of_sample, bins='auto')
+#plt.plot(E_theoretical, n_loops*DOS, 'r-o')
+plt.xlabel('Energy')
+plt.ylabel('Occurences')
+plt.title(f"{n_spins} spins, {5000} loops, beta = {beta}")
+
+plt.figure()
+plt.plot(f_loc)
+plt.axhline(y=F_ising, color='g', ls=':', label=r'$\mathcal{F}(\{ b_0 \})$')
+plt.ylabel(r'$\mathcal{F}$')
+plt.xlabel('Step')
+plt.title(f"{n_spins} spins, {5000} loops, beta = {beta}")
+
+
+plt.show();
+
